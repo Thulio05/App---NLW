7c40dd6b4d20b160a435dc0335f1256b89db3166
Thulio <thulioaleixo@gmail.com>
2024-09-14 20:17:27 -0300

    Fundamentos 2

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ console.log("Teste de console.log");
 // VARIAVEL MUTÁVEL = let + NOME
 // VARIAVEL IMUTÁVEL = const + NOME
 // NOVO ESCOPO = { + NOME + }
+// METODO DE OBJETO = NOME + . + NOME
+// FUNÇÃO = function + NOME + ( + PARAMETROS + ) + { + COMANDOS + }
+
+
 
 let escrita = "Teste de variável";
 let valor = 3
@@ -19,5 +23,13 @@ console.log(escrita + "\nValor definido = " + valor);
     console.log(escritaconstante + " <- Funcionou \n");
 }
 
-
 console.log(escritaconstante + " <- Funcionou \n");
+
+let meta = {
+    value : 'Ler um livro por mês',
+    checked : false
+}
+
+console.log(meta.value + " <- Funcionou \n");
+
+const criarMeta = () => {};
